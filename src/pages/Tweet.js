b356f7252589a6b0622a99c1f1fcba3fc02919d7
SarangKumar <sarangkumar1578@gmail.com>
2022-11-14 14:31:18 +0530

    Merged with main
    Added the tweet component. Added new images in the user_profile page. Edited the profile page

--- src/pages/Tweet.js
+++ src/pages/Tweet.js
@@ -1,2 +1,12 @@
+import React from 'react'
+import TweetCard from "../components/TweetCard.jsx"
+
+const Tweet = () => {
+  return (
+    <div>
+    <TweetCard />
+    </div>
+  )
+}
 
 export default Tweet
